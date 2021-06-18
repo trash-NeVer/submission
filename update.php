0f5f0524c37b906deb3bf403463afd8ac081325d
DESKTOP-CG7I5KV\younjkim <objection@kakao.com>
2021-06-19 00:13:18 +0900

    이름 수정

---
+++ update.php
@@ -0,0 +1,40 @@
+﻿<?php
+   $con=mysqli_connect("localhost", "root", "password", "neverdb") or die("MySQL 접속 실패 !!");
+   $sql ="SELECT * FROM userTBL WHERE userID='".$_GET['userID']."'";
+
+   $ret = mysqli_query($con, $sql);   
+   if($ret) {
+	   $count = mysqli_num_rows($ret);
+	   if ($count==0) {
+		   echo $_GET['userID']." 아이디의 회원이 없음!!!"."<br>";
+		   echo "<br> <a href='mypage.html'> <--돌아가기</a> ";
+		   exit();	
+	   }		   
+   }
+   else {
+	   echo "데이터 조회 실패!!!"."<br>";
+	   echo "실패 원인 :".mysqli_error($con);
+	   echo "<br> <a href=''> <--돌아가기</a> ";
+	   exit();
+   }   
+   $row = mysqli_fetch_array($ret);
+   
+   $userPassword = $row["userPassword"];
+   
+?>
+
+<HTML>
+<HEAD>
+<META http-equiv="content-type" content="text/html; charset=utf-8">
+</HEAD>
+<BODY>
+
+<h1> 회원 정보 수정 </h1>
+<FORM METHOD="post"  ACTION="update_result.php">
+	비밀번호 : <INPUT TYPE ="text" NAME="userPassword" VALUE=<?php echo $userPassword ?> READONLY> <br>
+	<BR><BR>
+	<INPUT TYPE="submit"  VALUE="정보 수정">
+</FORM>
+
+</BODY>
+</HTML>
---
+++ update_result.php
@@ -0,0 +1,23 @@
+﻿<?php
+   $con=mysqli_connect("localhost", "root", "1234", "sqlDB") or die("MySQL 접속 실패 !!");
+
+
+   $userPassword = $_POST["userPassword"];
+
+   
+   $sql ="UPDATE userTBL SET userPassword='".$userPassword."';
+   
+   $ret = mysqli_query($con, $sql);
+ 
+    echo "<h1> 회원 정보 수정 결과 </h1>";
+   if($ret) {
+	   echo "데이터가 성공적으로 수정됨.";
+   }
+   else {
+	   echo "데이터 수정 실패!!!"."<br>";
+	   echo "실패 원인 :".mysqli_error($con);
+   } 
+   mysqli_close($con);
+   
+   echo "<br> <a href='mypage.html'> <--돌아가기</a> ";
+?>
