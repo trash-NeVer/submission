dd38ed95a5aff8670181fcbadacdf03281d02cd7
DESKTOP-CG7I5KV\younjkim <objection@kakao.com>
2021-06-19 00:02:08 +0900

    no message

---
+++ final.php
@@ -0,0 +1,42 @@
+ <?php include "database.php"; ?>
+<?php session_start(); ?>
+<?php
+	//Create Select Query
+	$query="select * from shouts order by time desc limit 100";
+	$shouts = mysqli_query($con,$query);
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Quizzer!</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
+      <header>
+        <div class="container">
+          <h1>PHP Quizzer</h1>
+	</div>
+      </header>
+
+
+      <main>
+	<div class="container">
+	     <h2>You are Done!</h2>
+	     <p>Congrats! You have completed the test</p>
+	     <p>Final socre: <?php echo $_SESSION['score']; ?></p>
+	     <a href="question.php?n=1" class="start">Take Test Again</a>
+	     <?php session_destroy(); ?>
+	</div>
+      </main>
+
+
+    <footer>
+      <div class="container">
+      	   Copyright neverdb
+      </div>
+    </footer>
+  </body>
+</html>
